a6af21d5e85f252979c245e11cbbdc236454e094
Jillian <jillian.e.rowe@gmail.com>
2021-10-04 09:51:15 +0300

    adding public form view

--- fab_coreui_theme/fab_coreui_theme.py
+++ fab_coreui_theme/fab_coreui_theme.py
@@ -1,24 +1,31 @@
 """Main module."""
 
 from flask_appbuilder import (
-    action, aggregate_count, BaseView, expose, GroupByChartView, has_access, ModelView
+    action,
+    aggregate_count,
+    BaseView,
+    expose,
+    GroupByChartView,
+    has_access,
+    ModelView,
 )
 from flask import Blueprint, request, jsonify, Response, session
-from flask_appbuilder.views import MultipleView, SimpleFormView
+from flask_appbuilder.views import MultipleView, SimpleFormView, PublicFormView
 import os
 
-TEMPLATE_FOLDER = os.path.join(os.path.dirname(__file__), 'templates')
-STATIC_FOLDER = os.path.join(os.path.dirname(__file__), 'static')
-STATIC_URL_PATH = '/static/coreui'
+TEMPLATE_FOLDER = os.path.join(os.path.dirname(__file__), "templates")
+STATIC_FOLDER = os.path.join(os.path.dirname(__file__), "static")
+STATIC_URL_PATH = "/static/coreui"
 
 # Use the Flask Blueprint to create an appized app and setup our routing
 # https://flask.palletsprojects.com/en/2.0.x/blueprints/#templates
 
 coreui_bp = Blueprint(
-    'coreui_theme', __name__,
+    "coreui_theme",
+    __name__,
     template_folder=TEMPLATE_FOLDER,
     static_folder=STATIC_FOLDER,
-    static_url_path=STATIC_URL_PATH
+    static_url_path=STATIC_URL_PATH,
 )
 
 
@@ -32,15 +39,22 @@ class CoreUIModelView(ModelView):
     template_folder = TEMPLATE_FOLDER
     static_folder = STATIC_FOLDER
     static_url_path = STATIC_URL_PATH
-    form_template = 'coreui/general/model/edit.html'
-    edit_template = 'coreui/general/model/edit.html'
-    list_template = 'coreui/general/model/list.html'
-    show_template = 'coreui/general/model/show.html'
-    add_template = 'coreui/general/model/add.html'
+    form_template = "coreui/general/model/edit.html"
+    edit_template = "coreui/general/model/edit.html"
+    list_template = "coreui/general/model/list.html"
+    show_template = "coreui/general/model/show.html"
+    add_template = "coreui/general/model/add.html"
 
 
 class CoreUISimpleFormView(SimpleFormView):
     template_folder = TEMPLATE_FOLDER
     static_folder = STATIC_FOLDER
     static_url_path = STATIC_URL_PATH
-    form_template = 'coreui/general/model/edit.html'
+    form_template = "coreui/general/model/edit.html"
+
+
+class CoreUIPublicFormView(PublicFormView):
+    template_folder = TEMPLATE_FOLDER
+    static_folder = STATIC_FOLDER
+    static_url_path = STATIC_URL_PATH
+    form_template = "coreui/general/model/edit.html"
--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 import pkg_resources
 import versioneer
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = ["Flask-AppBuilder>=1.5.0"]
@@ -28,29 +28,29 @@ test_requirements = [
 
 setup(
     author="Jillian Rowe",
-    author_email='jillian@dabbleofdevops.com',
-    python_requires='>=3.6',
+    author_email="jillian@dabbleofdevops.com",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="CoreUI theme for FlaskAppbuilder",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='fab_coreui_theme',
-    name='fab_coreui_theme',
-    packages=find_packages(include=['fab_coreui_theme', 'fab_coreui_theme.*']),
-    test_suite='tests',
+    keywords="fab_coreui_theme",
+    name="fab_coreui_theme",
+    packages=find_packages(include=["fab_coreui_theme", "fab_coreui_theme.*"]),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/Dabble-of-DevOps-BioHub/fab_coreui_theme',
+    url="https://github.com/Dabble-of-DevOps-BioHub/fab_coreui_theme",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     zip_safe=False,
